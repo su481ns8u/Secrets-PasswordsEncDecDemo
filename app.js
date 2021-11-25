1c26b25238f71e76983bb343eae0dbb385ab2c37
su481ns8u <sghinmine54@gmail.com>
2021-11-25 09:41:51 +0530

    ADDED bcrypt encryption and authentication

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 const app = express()
 
@@ -32,16 +33,19 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
     const username = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
 
     User.findOne({ email: username }, (err, foundUser) => {
         if (err) {
             console.log(err)
+            console.log("You fucked")
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets")
-                }
+                bcrypt.compare(password, foundUser.password, (error, result) => {
+                    if (result === true) {
+                        res.render("secrets")
+                    }
+                })
             }
         }
     })
@@ -52,17 +56,22 @@ app.get("/register", (req, res) => {
 })
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+    
+        newUser.save((err) => {
+            if (err)
+                console.log(err)
+            else
+                res.render("secrets")
+        })
     })
 
-    newUser.save((err) => {
-        if (err)
-            console.log(err)
-        else
-            res.render("secrets")
-    })
 })
 
 app.listen(3000, () => {
